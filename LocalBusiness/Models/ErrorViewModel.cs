b54a0c3f22fa4c9f887612e4c7c73ee8be3de596
Alex Shevlin <alexshevlin1@gmail.com>
2022-08-19 09:06:49 -0700

    initial commit

---
+++ LocalBusiness/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LocalBusiness.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
