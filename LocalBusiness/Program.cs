e3101b5db293ec6e93ada6922eb5e7f330383dcb
Alex Shevlin <alexshevlin1@gmail.com>
2022-08-19 10:08:36 -0700

    update program.cs.

--- LocalBusiness/Program.cs
+++ LocalBusiness/Program.cs
@@ -1,27 +1,72 @@
-var builder = WebApplication.CreateBuilder(args);
+using LocalBusiness.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace LocalBusiness
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<LocalBusinessContext>(opt =>
+        opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { 
+          Title = "Local Businesses", 
+          Version = "v1",
+          Description = "An API containing a list of local businesses",
+          TermsOfService = new Uri("https://example.com/terms"),
+          Contact = new OpenApiContact
+          {
+              Name = "Alex Shevlin",
+              Email = string.Empty,
+              Url = new Uri("https://github.com/alexshevlin"),
+          }
+        });
+      });
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+        app.UseSwagger();
+
+        app.UseSwaggerUI(c => 
+        {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Local Businesses v1");
+        c.RoutePrefix = string.Empty;
+        });
+
+      // app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
